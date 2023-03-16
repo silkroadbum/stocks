ac8bb711eafc4e9451dd4a4e203789d67a35fa2b
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-17 02:41:36 +0600

    Стилизация

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -3,24 +3,27 @@ import { headers } from '../../const';
 
 function Table({ filteredStocks, step }) {
   return (
-    <div className="table-wrapper">
-      <table className="table">
-        <thead>
-          <tr>
-            {headers.map((item, i) => (
-              <th key={i} className="table__header">
-                {item}
-              </th>
+    <>
+      <h1 className="title">STOCKS</h1>
+      <div className="table-wrapper">
+        <table className="table">
+          <thead>
+            <tr>
+              {headers.map((item, i) => (
+                <th key={i} className="table__header">
+                  {item}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {filteredStocks.slice(step, step + 10).map((obj, i) => (
+              <Row key={obj.symbol} index={i + 1 + step} {...obj} />
             ))}
-          </tr>
-        </thead>
-        <tbody>
-          {filteredStocks.slice(step, step + 10).map((obj, i) => (
-            <Row key={obj.symbol} index={i + 1 + step} {...obj} />
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
