58b2e3da8a2bff22679c3b7bd906add21d5dec75
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-17 02:42:37 +0600

    Стилизация

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -4,6 +4,7 @@ import { headers } from '../../const';
 function Table({ filteredStocks, step }) {
   return (
     <>
+      <h1 className="title">STOCKS</h1>
       <div className="table-wrapper">
         <table className="table">
           <thead>
