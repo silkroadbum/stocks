f317a708970e00db6a2f13f7adf9823d7a4a6fd6
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-03-16 22:23:14 +0600

    удален лишний стиль

--- src/components/row/row.jsx
+++ src/components/row/row.jsx
@@ -13,16 +13,16 @@ function Row({
 }) {
   return (
     <tr className="table__row">
-      <td className="table__cell table__cell--center">{index}</td>
+      <td className="table__cell">{index}</td>
       <td className="table__cell">{symbol}</td>
       <td className="table__cell">{companyName}</td>
-      <td className="table__cell table__cell--center">{latestPrice}</td>
-      <td className="table__cell table__cell--center">{iexOpen}</td>
-      <td className="table__cell table__cell--center">{iexClose}</td>
-      <td className="table__cell table__cell--center">{change}</td>
-      <td className="table__cell table__cell--center">{changePercent}%</td>
+      <td className="table__cell">{latestPrice}</td>
+      <td className="table__cell">{iexOpen}</td>
+      <td className="table__cell">{iexClose}</td>
+      <td className="table__cell">{change}</td>
+      <td className="table__cell">{changePercent}%</td>
       <td className="table__cell">{currency}</td>
-      <td className="table__cell table__cell--center">{peRatio}</td>
+      <td className="table__cell">{peRatio}</td>
       <td className="table__cell">{primaryExchange}</td>
     </tr>
   );
